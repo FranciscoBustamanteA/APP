7e4936c83e92ea1339f9485e1900cea32948d0c6
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-04 10:34:55 -0300

    Web_Gx.py

--- Web_Gx.py
+++ Web_Gx.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+st.set_page_config(page_title="EnelGx_Gas", page_icon=":tada:", layout="wide")
 
 # ---- HEADER SECTION ----
 with st.container():
     st.subheader("Bienvenido,  :wave:")
-    st.title("Esta es la pagina repositorio de Planificación, Operación y Performance Gas and Fuels")
+    st.title("Esta es la pagina de Planificación, Operación y Performance Gas and Fuels")
     st.write(
         "Aqui iran las aplicaciones web desarrolladas para las tareas del área, buscando optimizar las tareas diarias"
     )
