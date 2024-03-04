a2bfe72a2c83887f33469f783c47b9da127cd4eb
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-04 11:38:06 -0300

    Web_Gx.py

--- Web_Gx.py
+++ Web_Gx.py
@@ -1,15 +1,24 @@
 import streamlit as st 
 
+# Definir credenciales
+USERNAME = "fba"
+PASSWORD = "fba"
 
-# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Enel", page_icon=":tada:", layout="wide")
+# Autenticación
+username_input = st.sidebar.text_input("Usuario")
+password_input = st.sidebar.text_input("Contraseña", type="password")
 
-# ---- HEADER SECTION ----
-with st.container():
+# Verificar credenciales
+if username_input == USERNAME and password_input == PASSWORD:
+    st.sidebar.success("¡Autenticación exitosa!")
+    
+    # ---- HEADER SECTION ----
     st.subheader("Bienvenido,  :wave:")
     st.title("Esta es la pagina repositorio de Planificación, Operación y Performance Gas and Fuels")
     st.write(
-        "Aqui iran las aplicaciones web desarrolladas para las tareas del área, buscando optimizar las tareas diarias"
+        "Aquí irán las aplicaciones web desarrolladas para las tareas del área, buscando optimizar las tareas diarias"
     )
-    st.write("cualquier problema contactar con francisco.bustamante@enel.com")  
-         
+    st.write("Cualquier problema contactar con francisco.bustamante@enel.com")
+
+else:
+    st.sidebar.error("Nombre de usuario o contraseña incorrectos.")
