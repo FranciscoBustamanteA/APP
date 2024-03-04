5df02ebadf792ff2c29efe1a4e09b5feb583ae23
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-04 11:54:23 -0300

    Update Web_Gx.py

--- Web_Gx.py
+++ Web_Gx.py
@@ -20,18 +20,14 @@ def pagina_opcion2():
     st.write("Contenido de la opción 2")
 
 def main():
-    # Siempre mostrar la página de inicio primero
-    pagina_inicio()
-
     # Autenticación
     username_input = st.sidebar.text_input("Usuario")
     password_input = st.sidebar.text_input("Contraseña", type="password")
     is_authenticated = False
 
-    # Verificar credenciales solo si se han ingresado valores
-    if username_input and password_input:
-        if username_input == USERNAME and password_input == PASSWORD:
-            is_authenticated = True
+    # Verificar credenciales
+    if username_input == USERNAME and password_input == PASSWORD:
+        is_authenticated = True
 
     if is_authenticated:
         st.sidebar.success("¡Autenticación exitosa!")
@@ -41,7 +37,7 @@ def main():
         seleccion = st.sidebar.radio("Ir a", ("Inicio", "Opción 1", "Opción 2"))
 
         if seleccion == "Inicio":
-            pass  # Ya se mostró la página de inicio
+            pagina_inicio()
         elif seleccion == "Opción 1":
             pagina_opcion1()
         elif seleccion == "Opción 2":
