b8a8572efd1a0be4cc5cf6ed7d84659139a84956
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-04 10:21:51 -0300

    Web_Gx.py

---
+++ Web_Gx.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hi, I am Sven :wave:")
+    st.title("A Data Analyst From Germany")
+    st.write(
+        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+    )
+    st.write("[Learn More >](https://pythonandvba.com)")
