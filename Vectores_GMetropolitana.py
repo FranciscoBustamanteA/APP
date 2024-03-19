16ab55dc2a3ae30c5af655da9fb6b966a4fd4b29
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-19 17:50:10 -0300

    Update Vectores_GMetropolitana.py

--- Vectores_GMetropolitana.py
+++ Vectores_GMetropolitana.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import pandas as pd
 def Calculo_Vectores_GMetropolitana():
-    st.title(
-        "Calculo Vectores Generadora Metropolitana")
+    st.title("Calculo Vectores Generadora Metropolitana")
+
+# Permitir que el usuario cargue un archivo Excel
+    archivo = st.file_uploader('Cargar archivo Excel', type=['xls', 'xlsx'])
+
+# Verificar si se ha cargado un archivo
+    if archivo is not None:
+        try:
+        # Leer el archivo Excel
+            df = pd.read_excel(archivo)
+        
+        # Mostrar los datos del archivo Excel
+            st.write('**Datos del archivo Excel:**')
+            st.write(df)
+        
+        except Exception as e:
+        # Manejar posibles errores al cargar el archivo
+            st.error(f'Ocurrió un error al cargar el archivo: {e}')
+
+    
