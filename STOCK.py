3c440f96680fc74ac17332fa01866d8919d2df3e
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-19 16:26:06 -0300

    Update STOCK.py

--- STOCK.py
+++ STOCK.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-
+# Installing Selenium with this shortcut
+!pip install kora
+!pip install selenium
 
 def stock():
     st.title("Calculo del STOCK disponible de Gas")
