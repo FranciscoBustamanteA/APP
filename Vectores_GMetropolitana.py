8e3d7c3925c3e8cc11a469c04973f6e7f0306b17
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-19 17:56:49 -0300

    Update Vectores_GMetropolitana.py

--- Vectores_GMetropolitana.py
+++ Vectores_GMetropolitana.py
@@ -6,6 +6,15 @@ def Calculo_Vectores_GMetropolitana():
 # Permitir que el usuario cargue un archivo Excel
     archivo = st.file_uploader('Cargar archivo Excel', type=['xls', 'xlsx'])
 
+# Intentar importar pandas con 'openpyxl'
+    try:
+        import pandas as pd
+        from pandas import ExcelFile
+        from pandas import ExcelWriter
+    except ImportError:
+        st.error("No se pudo importar pandas con openpyxl. Por favor, asegúrate de que pandas y openpyxl estén instalados.")
+        st.stop()
+    
 # Verificar si se ha cargado un archivo
     if archivo is not None:
         try:
