1a3ba7d9463b56e6bc7d466b755631ff1de13132
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-19 17:57:59 -0300

    Update Vectores_GMetropolitana.py

--- Vectores_GMetropolitana.py
+++ Vectores_GMetropolitana.py
@@ -1,25 +1,18 @@
 import streamlit as st
 import pandas as pd
+from openpyxl import load_workbook
+
 def Calculo_Vectores_GMetropolitana():
     st.title("Calculo Vectores Generadora Metropolitana")
 
 # Permitir que el usuario cargue un archivo Excel
     archivo = st.file_uploader('Cargar archivo Excel', type=['xls', 'xlsx'])
 
-# Intentar importar pandas con 'openpyxl'
-    try:
-        import pandas as pd
-        from pandas import ExcelFile
-        from pandas import ExcelWriter
-    except ImportError:
-        st.error("No se pudo importar pandas con openpyxl. Por favor, asegúrate de que pandas y openpyxl estén instalados.")
-        st.stop()
-    
 # Verificar si se ha cargado un archivo
     if archivo is not None:
         try:
         # Leer el archivo Excel
-            df = pd.read_excel(archivo)
+            df = pd.read_excel(archivo,engine='openpyxl')
         
         # Mostrar los datos del archivo Excel
             st.write('**Datos del archivo Excel:**')
