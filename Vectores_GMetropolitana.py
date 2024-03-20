76416ea480030329ac0349bd18f7a3531dd045cd
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-03-20 11:11:08 -0300

    Update Vectores_GMetropolitana.py

--- Vectores_GMetropolitana.py
+++ Vectores_GMetropolitana.py
@@ -5,14 +5,18 @@ def Calculo_Vectores_GMetropolitana():
     st.title("Calculo Vectores Generadora Metropolitana")
 
     # Permitir que el usuario cargue un archivo Excel
-    archivo = st.file_uploader('Cargar archivo Excel', type= 'xlsx')
+    archivo_vector = st.file_uploader('Cargar archivo Excel Vectores Sistema', type= 'xlsx')
 
+    #segunda archivo ADP
+    # Permitir que el usuario cargue un archivo Excel
+    archivo_ADP = st.file_uploader('Cargar archivo Excel ADP', type= 'xlsx')
+    
     # Verificar si se ha cargado un archivo
-    if archivo:
+    if archivo_vector:
         st.markdown('---')
         try:
             # Leer el archivo Excel
-            df = pd.read_excel(archivo, engine='openpyxl',sheet_name='Demanda')
+            df = pd.read_excel(archivo_vector, engine='openpyxl',sheet_name='Demanda')
             
             # Mostrar los datos del archivo Excel
             st.write('**Datos del archivo Excel:**')
