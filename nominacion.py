f887f392d92606f5675064b405a986f51ae39c2e
FranciscoBustamanteA <162136108+FranciscoBustamanteA@users.noreply.github.com>
2024-10-08 18:46:32 -0300

    Create nominacion.py

---
+++ nominacion.py
@@ -0,0 +1,7 @@
+# nominacion.py
+
+import streamlit as st
+
+def nominacion():
+    st.title("Nominación Gas 🚀")
+    st.write("A continuación seguir pasos para calcular y enviar las solicitudes de gas")
